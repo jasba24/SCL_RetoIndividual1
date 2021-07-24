8bc26b35365b82de4623a12601cf4035c9eb7660
jasba24 <johanbuitrago342@gmail.com>
2021-07-24 15:32:51 -0500

    feat: :fire: add new features to modal

--- src/components/Dashboard/DashboardModal/Modal.vue
+++ src/components/Dashboard/DashboardModal/Modal.vue
@@ -3,9 +3,28 @@
     <div @click="closeModal" class="modal__closeIcon">X</div>
     <h1 class="modal__title">Imagen de portada</h1>
     <form class="modal__form">
-      <label>URL de imagen</label>
-      <input ref="url" type="text" />
+      <div class="input__link">
+        <label>URL de imagen</label>
+        <input ref="url" type="text" />
+      </div>
       <button @click.prevent="toogleModal">Guardar</button>
+      <div class="form__options">
+        <div></div>
+        <p>o</p>
+        <div></div>
+      </div>
+      <div class="input__img">
+        <label class="input__file--description">Sube una imagen</label>
+        <label class="input__file--label"
+          >{{ chooseName }}
+          <input
+            ref="file"
+            @change="getFileUrl"
+            class="input__file"
+            type="file"
+            accept=".svg,.png,.jpg.jpeg,.gif"
+        /></label>
+      </div>
     </form>
   </section>
 </template>
@@ -18,12 +37,29 @@ const store = useStore();
 
 const url = ref(null);
 
+const file = ref(null);
+
+let fileUrl = ref("");
+
+let chooseName = ref("Escoge una imagen");
+
+const getFileUrl = () => {
+  const objectURL = URL.createObjectURL(file.value.files[0]);
+
+  console.log(file.value.files.length === 0);
+
+  chooseName.value =
+    file.value.files.length === 0 ? "Escoge una imagen" : "Imagen Escogida";
+
+  return objectURL ? (fileUrl = objectURL) : (fileUrl = url.value.value);
+};
+
 const closeModal = () => {
   store.commit("showModal", false);
 };
 
 const toogleModal = () => {
-  store.commit("setProfileBgUrl", url.value.value);
+  store.commit("setProfileBgUrl", fileUrl);
   store.commit("showModal");
 };
 </script>
@@ -40,13 +76,13 @@ secondary-color = #569b51
 .modal__title
   margin 16px 0 20px
 .modal__form
-  position relative
+  margin-bottom 10px
   & input
     border 1px solid #39424E
     border-width 2px
     border-radius 5px
     background-color primary-color
-    width 98%
+    width 97%
     height 50px
     font-size 16px
     padding-left 10px
@@ -66,17 +102,45 @@ secondary-color = #569b51
     text-transform uppercase
     font-weight bold
     cursor pointer
-  & label
-    position absolute
-    background-color #232730
-    color #B5B5B5
-    top -10px
-    left 10px
-    padding 0 10px
-.modal__closeIcon
-  font-size 20px
+  & .form__options
+    display flex
+    justify-content center
+    align-items center
+    & div
+      border 1px solid #39424E
+      width 50%
+      margin-top 20px
+      margin-bottom 20px
+    & p
+      font-size 30px
+      margin 0 15px
+      color #b5b5b5
+label
   position absolute
-  right 30px
-  top 20px
+  background-color #232730
+  color #B5B5B5
+  padding 0 10px
+  top -10px
+  left 10px
+.input__link, .input__img
+  position relative
+.input__img
+  height 50px
+.input__file
+  display none
+.input__file--description
+  z-index 2
+.input__file--label
+  height 40px
+  width 477px
+  padding-top 10px
+  border 2px solid #39424E
+  border-radius 5px
+  top 0
+  left 0
+  color white
+  font-size 18px
+  font-weight bold
   cursor pointer
+  text-align center
 </style>
